334d6c067bd5d23be0df2ed34367ff590ab00338
Volker Schwaberow <volker@schwaberow.de>
2025-01-18 06:33:15 +0100

    feat: Add documentation for UuidFactory methods

--- src/uuid_factory.cc
+++ src/uuid_factory.cc
@@ -4,6 +4,13 @@
 
 namespace vscuuid {
 
+/**
+ * @brief Factory method to create a UUID generator based on the specified UUID type.
+ *
+ * @param type The type of UUID generator to create. This should be one of the values from the UuidType enum.
+ * @return std::unique_ptr<UuidGeneratorBase> A unique pointer to the created UUID generator.
+ * @throws std::invalid_argument if the specified UUID type is not supported.
+ */
 std::unique_ptr<UuidGeneratorBase> UuidFactory::Create(UuidType type) {
   switch (type) {
     case UuidType::V1:
@@ -27,6 +34,16 @@ std::unique_ptr<UuidGeneratorBase> UuidFactory::Create(UuidType type) {
   }
 }
 
+/**
+ * @brief Detects the version of a given UUID.
+ *
+ * This function checks the format of the provided UUID string and extracts the version number.
+ * The UUID must be in the standard 8-4-4-4-12 format (36 characters long with hyphens at positions 8, 13, 18, and 23).
+ *
+ * @param uuid The UUID string to check.
+ * @return The version number of the UUID (an integer between 1 and 8).
+ * @throws std::invalid_argument if the UUID format is invalid or the version is unknown.
+ */
 int UuidFactory::DetectVersion(const std::string& uuid) {
   if (uuid.length() != 36 || uuid[8] != '-' || uuid[13] != '-' || uuid[18] != '-' || uuid[23] != '-') {
     throw std::invalid_argument("Invalid UUID format");
